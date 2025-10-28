cee8dee6b4fea03334f7dd190cb36ce28c775fea
martasanchez99 <msasa@dtu.dk>
2025-10-28 10:46:54 +0100

    function2

---
+++ R/function_2.R
@@ -0,0 +1,12 @@
+#' Transcription
+#'
+#' @param x2 this function replace T by U
+#'
+#' @returns It returns the same sequence but changing T by U
+#' @export
+#'
+#' @examples
+function2 <- function(x2){
+  n3 <- gsub("T", "U", x2)
+  return(n3)
+}
---
+++ tests/testthat/test-function2.R
@@ -0,0 +1,5 @@
+
+
+test_that("my function is capable of changing T into U ", {
+  expect_equal(function2("TT"), "UU")
+})
